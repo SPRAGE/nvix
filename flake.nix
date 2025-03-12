ff2df662a114027666f8aeee859412aa44044d56
shaun <shauna.pai@gmail.com>
2025-03-13 04:43:31 +0530

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "A nixvim configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixvim.url = "github:nix-community/nixvim";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    blink-cmp = {
+      url = "github:saghen/blink.cmp";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixvim,
+      flake-utils,
+      nixpkgs,
+      self,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        nixvimLib = nixvim.lib.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        nixvim' = nixvim.legacyPackages.${system};
+        nixvimModule = {
+          inherit pkgs;
+          module = import ./config; # import the module directly
+          # You can use `extraSpecialArgs` to pass additional arguments to your module files
+          extraSpecialArgs = {
+            inherit inputs self;
+          } // import ./lib { inherit pkgs; };
+        };
+        nvim = nixvim'.makeNixvimWithModule nixvimModule;
+      in
+      {
+        checks = {
+          # Run `nix flake check` to verify that your config is not broken
+          default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+        };
+
+        # Lets you run `nix run` to start nixvim
+        packages.default = nvim;
+      }
+    );
+}
