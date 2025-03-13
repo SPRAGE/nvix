09e69ef846305a2ac52a775954e2359d1930a61b
shaun <shauna.pai@gmail.com>
2025-03-13 05:58:08 +0530

    Feat: Gruvbox Theme

--- config/plugins/theme/default.nix
+++ config/plugins/theme/default.nix
@@ -1,24 +1,17 @@
 {
   colorschemes = {
-    kanagawa = {
+    gruvbox = {
       enable = true;
       settings = {
         background = {
-          light = "lotus";
-          dark = "wave";
+          light = "light";
+          dark = "dark";
         };
-        colors = {
-          palette = { };
-          theme = {
-            wave = { };
-            lotus = { };
-            dragon = { };
-            all = { };
-          };
-        };
+        contrast = "hard"; # can be "hard", "soft" or empty string
         terminal_colors = true;
-        transparent_background = true;
+        transparent_mode = true;
         undercurl = true;
+        bold = true;
         integrations = {
           cmp = true;
           flash = true;
@@ -58,3 +51,65 @@
     };
   };
 }
+
+#
+# {
+#   colorschemes = {
+#     gruvbox = {
+#       enable = true;
+#       settings = {
+#         background = {
+#           light = "lotus";
+#           dark = "wave";
+#         };
+#         colors = {
+#           palette = { };
+#           theme = {
+#             wave = { };
+#             lotus = { };
+#             dragon = { };
+#             all = { };
+#           };
+#         };
+#         terminal_colors = true;
+#         transparent_background = true;
+#         undercurl = true;
+#         integrations = {
+#           cmp = true;
+#           flash = true;
+#           fidget = true;
+#           gitsigns = true;
+#           indent_blankline.enabled = true;
+#           lsp_trouble = true;
+#           mini.enabled = true;
+#           neotree = true;
+#           noice = true;
+#           notify = true;
+#           telescope.enabled = true;
+#           treesitter = true;
+#           treesitter_context = true;
+#           which_key = true;
+#           native_lsp = {
+#             enabled = true;
+#             inlay_hints = {
+#               background = true;
+#             };
+#             virtual_text = {
+#               errors = [ "italic" ];
+#               hints = [ "italic" ];
+#               information = [ "italic" ];
+#               warnings = [ "italic" ];
+#               ok = [ "italic" ];
+#             };
+#             underlines = {
+#               errors = [ "underline" ];
+#               hints = [ "underline" ];
+#               information = [ "underline" ];
+#               warnings = [ "underline" ];
+#             };
+#           };
+#         };
+#       };
+#     };
+#   };
+# }
