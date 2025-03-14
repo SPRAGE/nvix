263509e363d2fb70ac0e88a9a63f43c8eae87cd8
shaun <shauna.pai@gmail.com>
2025-03-14 06:46:40 +0530

    Feat: Clipboard

--- config/settings.nix
+++ config/settings.nix
@@ -98,7 +98,7 @@
 
         -- Neovide Fonts
         o.guifont = "MonoLisa Trial:Medium:h15"
-        o.clipboard = "unnamedplus"
+        o.clipboard = "unnamedplus" -- unnamed plus
         -- o.guifont = "CommitMono:Medium:h15"
         -- o.guifont = "JetBrainsMono Nerd Font:h14:Medium:i"
         -- o.guifont = "FiraMono Nerd Font:Medium:h14"
