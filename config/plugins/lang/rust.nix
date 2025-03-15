322051cd7386bd4c22ec31d41b41e4417bfe06bd
shaun <shauna.pai@gmail.com>
2025-03-15 14:26:26 +0530

    Fix: Rust LSP

--- config/plugins/lang/rust.nix
+++ config/plugins/lang/rust.nix
@@ -1,7 +1,5 @@
 { pkgs, ... }:
 {
-  # Needed for RustPlay
-  extraPlugins = with pkgs.vimPlugins; [ webapi-vim ];
 
   plugins = {
     rustaceanvim = {
@@ -29,7 +27,7 @@
 
               checkOnSave = true;
               check = {
-                command = "check";
+                command = "clippy";
                 features = "all";
               };
 
@@ -66,3 +64,72 @@
     };
   };
 }
+
+#
+#
+# { pkgs, ... }:
+# {
+#
+#   plugins = {
+#     rustaceanvim = {
+#       enable = true;
+#       settings = {
+#
+#         dap = {
+#           autoloadConfigurations = true;
+#         };
+#
+#         server = {
+#           enable = true;
+#           autoStart = true;
+#           default_settings = {
+#             rust-analyzer = {
+#               cargo = {
+#                 buildScripts.enable = true;
+#                 features = "all";
+#               };
+#
+#               diagnostics = {
+#                 enable = true;
+#                 styleLints.enable = true;
+#               };
+#
+#               checkOnSave = true;
+#               check = {
+#                 command = "clippy";
+#                 features = "all";
+#               };
+#
+#               files = {
+#                 excludeDirs = [
+#                   ".cargo"
+#                   ".direnv"
+#                   ".git"
+#                   "node_modules"
+#                   "target"
+#                 ];
+#               };
+#
+#               inlayHints = {
+#                 bindingModeHints.enable = true;
+#                 closureStyle = "rust_analyzer";
+#                 closureReturnTypeHints.enable = "always";
+#                 discriminantHints.enable = "always";
+#                 expressionAdjustmentHints.enable = "always";
+#                 implicitDrops.enable = true;
+#                 lifetimeElisionHints.enable = "always";
+#                 rangeExclusiveHints.enable = true;
+#               };
+#
+#               procMacro = {
+#                 enable = true;
+#               };
+#
+#               rustc.source = "discover";
+#             };
+#           };
+#         };
+#       };
+#     };
+#   };
+# }
